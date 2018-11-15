77163170853c44d5c9cad0ef1e314e47f28cce30
Luc <lucwaeijen@gmail.com>
2018-11-15 18:56:39 +0100

    Fix broken channel swap in CPU version of preprocessing after porting to blobs instead of images

--- preprocessing.cu
+++ preprocessing.cu
@@ -8,8 +8,8 @@ void cpu_preprocess(BLOB* img){
         for(int x=0;x<img->w;x++){
             float R              =  (blob_data(img,0,y,x)-123.680f)*0.017f; //R
             blob_data(img, 1,y,x) = (blob_data(img,1,y,x)-116.779f)*0.017f; //G
-            blob_data(img, 2,y,x) = (blob_data(img,2,y,x)-103.939f)*0.017f; //B
-            blob_data(img, 0,y,x) = R;
+            blob_data(img, 0,y,x) = (blob_data(img,2,y,x)-103.939f)*0.017f; //B
+            blob_data(img, 2,y,x) = R;
          }
 }
 
