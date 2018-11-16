11f902c326c2740b1c81c8e4c88670364f171f3b
Luc <lucwaeijen@gmail.com>
2018-11-16 11:15:45 +0100

    Add additional flags for higher compute capability

--- settings.mk
+++ settings.mk
@@ -35,6 +35,8 @@ DARWIN = $(strip $(findstring DARWIN, $(OSUPPER)))
 GENCODE_SM10    := -Wno-deprecated-gpu-targets -gencode arch=compute_10,code=sm_10
 GENCODE_SM20    := -Wno-deprecated-gpu-targets -gencode arch=compute_20,code=sm_20
 GENCODE_SM30    := -gencode arch=compute_30,code=sm_30
+GENCODE_SM35    := -gencode arch=compute_35,code=sm_35
+GENCODE_SM50    := -gencode arch=compute_50,code=sm_50
 
 #NOTE: Set this based on your device!!
 GENCODE_FLAGS   := $(GENCODE_SM20)
