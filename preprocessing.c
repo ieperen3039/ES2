51d5250df76e18f940dd417f411afdb2ea38a2bb
Jorrit Olthuis <j.j.olthuis@student.tue.nl>
2018-12-23 19:59:23 +0100

    Add missing curly-bracket pair

--- preprocessing.c
+++ preprocessing.c
@@ -4,7 +4,7 @@
 //CPU version
 void cpu_preprocess(BLOB* img) {
 	//Subtract mean RGB values, scale with 0.017, and swap RGB->BGR
-	for (int y = 0; y < img->h; y++)
+    for (int y = 0; y < img->h; y++) {
 		for (int x = 0; x < img->w; x++) {
 			float R = (blob_data(img,0,y,x)-123.680f)*0.017f; //R
 			blob_data(img, 1,y,x) = (blob_data(img,1,y,x)-116.779f)*0.017f;//G
@@ -12,4 +12,5 @@ void cpu_preprocess(BLOB* img) {
 			blob_data(img, 2,y,x) = R;
 		}
 	}
+}
 
